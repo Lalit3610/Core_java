1e57436b9df6a2693086a6430053981b2eaac145
Lalit3610 <109783524+Lalit3610@users.noreply.github.com>
2022-09-09 10:49:45 +0530

    Add files via upload

---
+++ FahToCel.java
@@ -0,0 +1,17 @@
+class FahToCal
+{
+public static void main(String Args[])
+{
+Scanner s=new Scanner(System.in);
+
+Float f;
+double cel;
+Syestem.out.println("enter the temp in fahrenhit");
+
+f= s.nextFloat();
+Cel=(f-32)/1.8;
+
+Syestem.out.println("fahrenhit is :" +f);
+Syestem.out.println("celcius :is" +cel);
+}
+}
